3d4d88608ce06320f30d17f28ab30be970c2d7ca
VictorGutierrez <vagutierrezv@uqvirtual.edu.co>
2023-05-25 10:48:15 -0500

    Se crean componentes Administrador y Maquina01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { AdministradorComponent } from './components/administrador/administrador.component';
+import { Maquina01Component } from './components/maquina01/maquina01.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    AdministradorComponent,
+    Maquina01Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/maquina01/maquina01.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maquina01',
+  templateUrl: './maquina01.component.html',
+  styleUrls: ['./maquina01.component.css']
+})
+export class Maquina01Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
